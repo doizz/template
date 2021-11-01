ce0d0fd2ccc98a231e00ff82e09744d167300004
DESKTOP-GA8CAA3\JUNG <doizz@naver.com>
2021-11-01 20:33:06 +0900

    Initial commit

---
+++ src/main/java/com/function/template/FunctionTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.function.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionTemplateApplication.class, args);
+    }
+
+}
