479aab1843b30354bd91bc5694ed696e93ec16e0
DESKTOP-GA8CAA3\JUNG <doizz@naver.com>
2021-11-04 23:07:19 +0900

    게시글 등록 화면 추가

--- src/main/java/com/function/template/mapper/BoardMapper.java
+++ src/main/java/com/function/template/mapper/BoardMapper.java
@@ -9,4 +9,5 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface BoardMapper {
     List<BoardDto> selectBoardList() throws Exception;
+    void insertBoard(BoardDto board) throws Exception;
 }
