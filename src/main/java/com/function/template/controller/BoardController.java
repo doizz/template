54ef1d6de91eee37402f332dca46001c3a8cfa4c
DESKTOP-GA8CAA3\JUNG <doizz@naver.com>
2021-11-05 22:20:17 +0900

    게시판 view 추가

--- src/main/java/com/function/template/controller/BoardController.java
+++ src/main/java/com/function/template/controller/BoardController.java
@@ -5,6 +5,7 @@ import com.function.template.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -32,5 +33,16 @@ public class BoardController {
     public String insertBoard(BoardDto board) throws Exception {
         boardService.insertBoard(board);
         return "redirect:/board/openBoardList.do";
+    }
+
+
+    @RequestMapping("/board/openBoardDetail.do")
+    public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception {
+        ModelAndView mv = new ModelAndView("/board/boardDetail");
+
+        BoardDto board = boardService.selectBoardDetail(boardIdx);
+        mv.addObject("board", board);
+
+        return mv;
     }
 }
--- src/main/java/com/function/template/mapper/BoardMapper.java
+++ src/main/java/com/function/template/mapper/BoardMapper.java
@@ -10,4 +10,6 @@ import org.apache.ibatis.annotations.Mapper;
 public interface BoardMapper {
     List<BoardDto> selectBoardList() throws Exception;
     void insertBoard(BoardDto board) throws Exception;
+    void updateHitCount(int boardIdx) throws Exception;
+    BoardDto selectBoardDetail(int boardIdx) throws Exception;
 }
