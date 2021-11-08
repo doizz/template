a26c2f9c35a090975dc4932d7fbdaaae56339dcf
DESKTOP-GA8CAA3\JUNG <doizz@naver.com>
2021-11-08 22:13:45 +0900

    logback 설정

--- src/main/java/com/function/template/controller/BoardController.java
+++ src/main/java/com/function/template/controller/BoardController.java
@@ -7,15 +7,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+// ADD START
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// ADD END
 @Controller
 public class BoardController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private BoardService boardService;
 
     @RequestMapping("/board/openBoardList.do")
     public ModelAndView openBoardList() throws Exception {
+        log.debug("openBoardList");
+
         ModelAndView mv = new ModelAndView("/board/boardList");
 
         List<BoardDto> list = boardService.selectBoardList();
