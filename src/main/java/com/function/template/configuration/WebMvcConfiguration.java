fe31230c840e0c51490ed950d9f4cf2be56c7de5
DESKTOP-GA8CAA3\JUNG <doizz@naver.com>
2021-11-09 22:20:11 +0900

    인터셉터 설정

---
+++ src/main/java/com/function/template/configuration/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.function.template.configuration;
+
+
+import com.function.template.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}
